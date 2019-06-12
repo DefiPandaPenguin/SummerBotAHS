1ce26e1a113fd250a9598f48297a68576de97dad
Summer Camp <camp@ahschool.com>
2019-06-12 14:56:12 -0400

    del old ver

--- SummerBot/src/main/java/frc/robot/m.java
+++
@@ -1,9 +0,0 @@
-package frc.robot;
-import frc.robot.subsystems.*;
-
-public class Main {
-
-    public static Drivetrain drive = new Drivetrain();
-    public static Clock clock = new Clock();
-    
-}
